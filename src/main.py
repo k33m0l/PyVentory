5c7441933b3a88613dcdaf71f791f6f3f8a15801
k33m0l <amolnar4d41@gmail.com>
2025-02-25 19:18:56 +0100

    Basic db read and write

--- src/main.py
+++ src/main.py
@@ -11,6 +11,20 @@ connection = psycopg2.connect(
 )
 
 cursor = connection.cursor()
-cursor.execute("SELECT version();")
-record = cursor.fetchone()
-print("SQL version is ", record, "\n")
+
+# Create table
+cursor.execute("CREATE TABLE inventory (name varchar(255), count int);")
+
+# Add example data
+cursor.execute("INSERT INTO inventory (name, count) VALUES('Mayo', 1);")
+cursor.execute("INSERT INTO inventory (name, count) VALUES('Cheese', 3);")
+cursor.execute("INSERT INTO inventory (name, count) VALUES('Salami', 4);")
+
+# Query all data
+cursor.execute("SELECT * FROM inventory;")
+
+
+record = cursor.fetchall()
+
+for item in record:
+    print("Data from db: " + str(item))
