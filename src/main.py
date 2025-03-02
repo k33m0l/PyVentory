93502f60b70e0f5a23d6c8690381f6dc5c66d941
k33m0l <amolnar4d41@gmail.com>
2025-03-02 16:00:32 +0100

    Using cursor manager

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import psycopg2
 import os
+import operations.cursor
 import operations.reader
 import operations.writer
 
@@ -16,30 +17,26 @@ def connectToDB():
         print("Error connecting to database: " + str(err))
         raise
 
-def getCursor(conn1):
+# Create table
+def initDatabase(conn):
     try:
-        return conn1.cursor()
-    except psycopg2.Error as err:
-        print("Error connecting to database: " + str(err))
-        raise
-
-def initDatabase(cursor):
-    try:
-        # Create table
+        cursor = operations.cursor.CursorManager().create_cursor(conn)
         cursor.execute("CREATE TABLE inventory (name varchar(255), count int);")
     except psycopg2.Error as err:
         print("Failed to create database: " + str(err))
         raise
 
+# Init DB
 connection = connectToDB()
-cursor = getCursor(connection)
-initDatabase(cursor)
+initDatabase(connection)
 
+# Add sample data
 db_writer = operations.writer.Writer()
 db_writer.add_item(connection, "Mayo", 1)
 db_writer.add_item(connection, "Cheese", 3)
 db_writer.add_item(connection, "Salami", 4)
 
+# Read all data from db
 db_reader = operations.reader.Reader()
 for item in db_reader.readAll(connection):
     print("Data from db: " + str(item))
