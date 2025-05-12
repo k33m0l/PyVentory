bb083c706ed014e1176273321262bfa831d9a912
k33m0l <k33m0l@users.noreply.github.com>
2025-05-12 19:20:29 +0200

    Add mock verification

--- src/service/items.py
+++ src/service/items.py
@@ -3,18 +3,15 @@
 from db_ops import connect_to_db
 from operations.reader import Reader
 
-def fetch_all(connection):
-    db_reader = Reader()
-    return db_reader.read_all(connection)
-
-def run():
+def fetch_all():
     conn = connect_to_db()
-    response = fetch_all(conn)
+    db_reader = Reader()
+    response = db_reader.read_all(conn)
     conn.close()
     return response
 
 if __name__ == "__main__":
     # input will go here
-    response = run()
+    response = fetch_all()
     for item in response:
         print("Data from db: " + str(item))
--- test/service/test_items.py
+++ test/service/test_items.py
@@ -2,20 +2,31 @@ import unittest
 from unittest.mock import MagicMock, patch
 import src.service.items as under_test
 
+SAMPLE_ITEM_1 = ("Item1", 10)
+SAMPLE_ITEM_2 = ("Item2", 100)
+SAMPLE_ITEM_3 = ("Item3", 3)
+
 class ItemsTest(unittest.TestCase):
 
     @patch("src.service.items.Reader")
     def test_fetch_all(self, reader_mock_class):
+        # GIVEN
         under_test.connect_to_db = MagicMock(name="connect_to_db")
         mock_conn = MagicMock()
         under_test.connect_to_db.return_value = mock_conn
 
         reader_mock = reader_mock_class.return_value
         reader_mock.conn = mock_conn
-        reader_mock.read_all.return_value = ["asd", "dsa"]
+        reader_mock.read_all.return_value = [SAMPLE_ITEM_1, SAMPLE_ITEM_2, SAMPLE_ITEM_3]
 
-        result = under_test.run()
-        self.assertEqual(["asd", "dsa"], result)
+        # WHEN
+        result = under_test.fetch_all()
+        expected = [SAMPLE_ITEM_1, SAMPLE_ITEM_2, SAMPLE_ITEM_3]
+
+        # THEN
+        self.assertEqual(expected, result)
+        under_test.connect_to_db.assert_called_once()
+        reader_mock.read_all.assert_called_once_with(mock_conn)
 
 if __name__ == '__main__':
     unittest.main()
