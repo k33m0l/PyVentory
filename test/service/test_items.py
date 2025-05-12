617044fdeafd077a806bc42fb46debb578ddd272
k33m0l <k33m0l@users.noreply.github.com>
2025-05-12 18:58:16 +0200

    Simple mocks

--- test/service/test_items.py
+++ test/service/test_items.py
@@ -1,11 +1,25 @@
 import unittest
-from src.service.items import dummy_method_test
+from unittest.mock import MagicMock, patch
+import src.service.items as under_test
 
 class ItemsTest(unittest.TestCase):
 
+    @patch("src.service.items.connect_to_db")
+    @patch("src.service.items.Reader")
+    def test_fetch_all(self, mock_connect_to_db, mock_reader_class):
+        mock_conn = MagicMock()
+        mock_connect_to_db.return_value = mock_conn
+
+        mock_reader = MagicMock()
+        mock_reader.read.return_value = "mocked data"
+        mock_reader_class.return_value = mock_reader
+
+        result = under_test.run()
+        self.assertEqual("", result)
+
     def test_sample(self):
-        result = dummy_method_test()
-        self.assertEqual("expected", result)
+        result = under_test.dummy_method_test()
+        self.assertEqual("dummy", result)
 
 if __name__ == '__main__':
     unittest.main()
