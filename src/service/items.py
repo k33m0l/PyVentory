55a9410c49e03b72e35b0694056f8af1db18538e
k33m0l <k33m0l@users.noreply.github.com>
2025-05-12 19:12:08 +0200

    Working unit test with mocks

--- src/service/items.py
+++ src/service/items.py
@@ -4,18 +4,17 @@ from db_ops import connect_to_db
 from operations.reader import Reader
 
 def fetch_all(connection):
-    # Read all data from db
     db_reader = Reader()
-    for item in db_reader.read_all(connection):
-        print("Data from db: " + str(item))
+    return db_reader.read_all(connection)
 
 def run():
     conn = connect_to_db()
-    fetch_all(conn)
+    response = fetch_all(conn)
     conn.close()
-
-def dummy_method_test():
-    return "dummy"
+    return response
 
 if __name__ == "__main__":
-    run()
+    # input will go here
+    response = run()
+    for item in response:
+        print("Data from db: " + str(item))
--- test/service/test_items.py
+++ test/service/test_items.py
@@ -4,22 +4,18 @@ import src.service.items as under_test
 
 class ItemsTest(unittest.TestCase):
 
-    @patch("src.service.items.connect_to_db")
     @patch("src.service.items.Reader")
-    def test_fetch_all(self, mock_connect_to_db, mock_reader_class):
+    def test_fetch_all(self, reader_mock_class):
+        under_test.connect_to_db = MagicMock(name="connect_to_db")
         mock_conn = MagicMock()
-        mock_connect_to_db.return_value = mock_conn
+        under_test.connect_to_db.return_value = mock_conn
 
-        mock_reader = MagicMock()
-        mock_reader.read.return_value = "mocked data"
-        mock_reader_class.return_value = mock_reader
+        reader_mock = reader_mock_class.return_value
+        reader_mock.conn = mock_conn
+        reader_mock.read_all.return_value = ["asd", "dsa"]
 
         result = under_test.run()
-        self.assertEqual("", result)
-
-    def test_sample(self):
-        result = under_test.dummy_method_test()
-        self.assertEqual("dummy", result)
+        self.assertEqual(["asd", "dsa"], result)
 
 if __name__ == '__main__':
     unittest.main()
