3c34e02ab52a9e5c526c432ea87f1b74265487e8
k33m0l <k33m0l@users.noreply.github.com>
2025-05-13 19:29:18 +0200

    Added unit tests for the db operations

---
+++ test/operations/test_db_ops.py
@@ -0,0 +1,96 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from src.objects.item import Item
+import src.operations.db_ops as under_test
+
+SAMPLE_ITEM_1 = (1, "Item1", 10)
+SAMPLE_ITEM_2 = (2, "Item2", 100)
+SAMPLE_ITEM_3 = (3, "Item3", 3)
+ITEM_WITHOUT_ID = Item("NewItem", 20)
+OS_ENVIRON = {
+    "DB_NAME": "temp",
+    "DB_USER": "tempuser",
+    "DB_PASS": "temppass",
+}
+
+class ItemsTest(unittest.TestCase):
+
+    @patch.dict("os.environ", OS_ENVIRON, clear=True)
+    def test_connect_to_db(self):
+        # GIVEN
+        expected_conn = MagicMock()
+        under_test.psycopg2.connect = MagicMock()
+        under_test.psycopg2.connect.return_value = expected_conn
+        
+        # WHEN
+        result = under_test.connect_to_db()
+        
+        # THEN
+        self.assertTrue(result.autocommit)
+        self.assertEqual(expected_conn, result)
+        under_test.psycopg2.connect.assert_called_once()
+
+    @patch("src.operations.db_ops.connect_to_db")
+    def test_create_table(self, mock_connect_to_db):
+        # GIVEN
+        mock_conn = MagicMock()
+        mock_connect_to_db.return_value = mock_conn
+        mock_cursor = MagicMock()
+        mock_conn.cursor.return_value = mock_cursor
+        
+        # WHEN
+        under_test.create_table()
+        
+        # THEN
+        mock_connect_to_db.assert_called_once()
+        mock_conn.cursor.assert_called_once()
+        mock_cursor.execute.assert_called_once()
+        mock_conn.close.assert_called_once()
+
+    @patch("src.operations.db_ops.connect_to_db")
+    def test_read_all_items(self, mock_connect_to_db):
+        # GIVEN
+        mock_conn = MagicMock()
+        mock_connect_to_db.return_value = mock_conn
+        mock_cursor = MagicMock()
+        mock_conn.cursor.return_value = mock_cursor
+        mock_cursor.fetchall.return_value = [SAMPLE_ITEM_1, SAMPLE_ITEM_2, SAMPLE_ITEM_3]
+
+        expected = [SAMPLE_ITEM_1, SAMPLE_ITEM_2, SAMPLE_ITEM_3]
+        
+        # WHEN
+        result = under_test.read_all_items()
+        
+        # THEN
+        self.assertEqual(expected, result)
+        mock_connect_to_db.assert_called_once()
+        mock_conn.cursor.assert_called_once()
+        mock_cursor.execute.assert_called_once_with("SELECT * FROM inventory;")
+        mock_cursor.fetchall.assert_called_once()
+        mock_conn.close.assert_called_once()
+
+    @patch("src.operations.db_ops.connect_to_db")
+    def test_add_item(self, mock_connect_to_db):
+        # GIVEN
+        mock_conn = MagicMock()
+        mock_connect_to_db.return_value = mock_conn
+        mock_cursor = MagicMock()
+        mock_conn.cursor.return_value = mock_cursor
+        
+        # WHEN
+        under_test.add_item(ITEM_WITHOUT_ID)
+
+        # THEN
+        mock_connect_to_db.assert_called_once()
+        mock_conn.cursor.assert_called_once()
+        mock_cursor.execute.assert_called_once_with(
+            "INSERT INTO inventory (name, count) VALUES(%(item_name)s, %(item_amount)s);",
+            {
+                "item_name": ITEM_WITHOUT_ID.name,
+                "item_amount": str(ITEM_WITHOUT_ID.amount),
+            }
+        )
+        mock_conn.close.assert_called_once()
+
+if __name__ == '__main__':
+    unittest.main()
